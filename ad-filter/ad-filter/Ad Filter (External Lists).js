f05f63dcfaa14014f23949ac2b0a78269d37c8f9
Stiffan-de <75042757+Stiffan-de@users.noreply.github.com>
2026-09-18 16:15:08 +0300

    Create Ad Filter (External Lists).js

---
+++ ad-filter/ad-filter/Ad Filter (External Lists).js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Ad Filter (External Lists)
+// @namespace    https://github.com/StiffMusic/stiffmusic-userscripts
+// @version      1.4.2
+// @description  Фильтр рекламы: внешние списки, автономное обновление, живая панель с таймером, кэш, свои правила, :has-text(), настраиваемые хоткеи.
+// @author       StiffMusic
+// @copyright    2026, StiffMusic (https://github.com/StiffMusic)
+// @license      PolyForm-Noncommercial-1.0.0; https://polyformproject.org/licenses/noncommercial/1.0.0/
+// @homepageURL  https://github.com/StiffMusic/stiffmusic-userscripts
+// @supportURL   https://github.com/StiffMusic/stiffmusic-userscripts/issues
+// @updateURL    https://raw.githubusercontent.com/StiffMusic/stiffmusic-userscripts/main/ad-filter/ad-filter.user.js
+// @downloadURL  https://raw.githubusercontent.com/StiffMusic/stiffmusic-userscripts/main/ad-filter/ad-filter.user.js
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @connect      easylist.to
+// @connect      raw.githubusercontent.com
+// @connect      cdn.jsdelivr.net
+// @connect      filters.adtidy.org
+// @run-at       document-idle
+// @noframes
+// ==/UserScript==
